6b4bb112353c96d6e95002ad06860ae52ce46dbb
Lazar Miric <lazarmiric@outlook.com>
2021-01-20 18:46:06 +0100

    Add project files.

---
+++ WebAPI/Controllers/UserController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private UserManager<User> _userManager;
+        private SignInManager<User> _signInManager;
+        public UserController(UserManager<User> userManager, SignInManager<User> signInManager)
+        {
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        [Route("Register")]
+        // registracija - ruta: http://localhost:58532/api/User/Register
+        public async Task<Object> RegisterUser(UserModel model)
+        {
+            var user = new User() {
+                UserName = model.UserName,
+                Email = model.Email,
+                FullName = model.FullName,
+                DateOfBirth = model.DateOfBirth
+            };
+            try
+            {
+                var results = await _userManager.CreateAsync(user, model.Password);
+                return Ok(results);
+
+            } 
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            
+        }
+    }
+}
---
+++ WebAPI/Migrations/20210119223456_addedDateOfBirth.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class addedDateOfBirth : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DateOfBirth",
+                table: "AspNetUsers",
+                type: "DateTime",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "AspNetUsers");
+        }
+    }
+}
